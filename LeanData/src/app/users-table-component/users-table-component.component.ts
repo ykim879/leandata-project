0de8f692dd0ebc95557ada67b43977d338bc3e9a
ykim8 <ykim879@gatech.edu>
2023-05-06 14:48:20 -0700

    First set up

---
+++ LeanData/src/app/users-table-component/users-table-component.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-table-component',
+  templateUrl: './users-table-component.component.html'
+})
+export class UsersTableComponentComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
